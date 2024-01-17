220c414b5c2c82b5f4500210b754a1af70db4e74
khalil khan <khalilkhankk3272711@gmail.com>
2024-01-17 12:53:53 +0500

    Aura

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../header/Header'
+
+const Dashboard = () => {
+  return (
+    <>
+    <Header/>
+    
+    
+    </>
+  )
+}
+
+export default Dashboard

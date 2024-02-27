31c7e29ae9c5a5019c628026db7019e2e0fdb318
khalil khan <khalilkhankk3272711@gmail.com>
2024-02-27 13:27:24 +0500

    Modify Sidebar ActiveLinks

--- src/components/reusebale components/HomeCard.js
+++ src/components/reusebale components/HomeCard.js
@@ -1,19 +1,42 @@
-import React from 'react'
-import listicon from '../images/listicon.png'
+import React from "react";
+import listicon from "../images/listicon.png";
 
-const HomeCard = ({profile,name,brandname,date,type}) => {
+const HomeCard = ({ profile, name, brandname, date, type }) => {
   return (
     <>
-     <div className="row main-items mx-3 mt-2">
-                <div className="col-md-3 text-start ps-4 name"><p className='p-0'><span><img className='me-3' src={profile} width={30} height={30} alt="" /></span> {name}</p></div>
-                <div className="col-md-2 text-start"><p>{date}</p></div>
-                <div className="col-md-2"><p>{brandname}</p></div>
-                <div className="col-md-3 text-center "><p>{type}</p></div>
-                <div className="col-md-2 text-end action" style={{paddingRight:"35px"}}><img src={listicon} alt="" /></div>
-            </div>
-    
+      <div className="row main-items mx-3 mt-2">
+        <div className="col-md-3 text-start ps-4 name">
+          <p className="p-0">
+            <span>
+              <img
+                className="me-3"
+                src={profile}
+                width={30}
+                height={30}
+                alt=""
+              />
+            </span>{" "}
+            {name}
+          </p>
+        </div>
+        <div className="col-md-2 text-start">
+          <p>{date}</p>
+        </div>
+        <div className="col-md-2">
+          <p>{brandname}</p>
+        </div>
+        <div className="col-md-3 text-center ">
+          <p>{type}</p>
+        </div>
+        <div
+          className="col-md-2  justify-content-end action d-flex align-items-center "
+          style={{ paddingRight: "35px" }}
+        >
+          <img src={listicon} alt="" />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default HomeCard
+export default HomeCard;
--- src/components/smallscreen/SmallScreenMsg.js
+++ src/components/smallscreen/SmallScreenMsg.js
@@ -1,21 +1,17 @@
-import React from 'react'
-import './smallscreen.css'
-import logo from '../images/logo.png'
+import React from "react";
+import "./smallscreen.css";
+import logo from "../images/logo.png";
 const SmallScreenMsg = () => {
   return (
     <>
-    
-    <div className="SmallScreenContainer container-fluid  ">
+      <div className="SmallScreenContainer container-fluid  ">
         <div>
-        <img src={logo} alt="" />
-            <p>
-                This Aura Admin Dashboard is only for Desktop size screens!!!
-            </p>
-         </div>
-
-    </div>
+          <img src={logo} alt="" />
+          <p>Only Desktop devices are supported!</p>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SmallScreenMsg
+export default SmallScreenMsg;
